60868235e4899403ef070d39bcd58a9c2c65f6d4
seoknyang <55523889+seoknyang@users.noreply.github.com>
2019-09-19 14:40:06 +0900

    Add files via upload

---
+++ arth.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	
+	printf("첫 번째수 :");
+	scanf("%d", &a);
+	printf("두 번째수 :");
+	scanf("%d", &b);
+	
+	printf("%d + %d = %d\n", a ,b ,(a+b));
+	printf("%d - %d = %d\n", a ,b ,(a-b));
+	printf("%d * %d = %d\n", a ,b ,(a*b));
+	printf("%d / %d = %f\n", a ,b ,((float)a/b));
+	return 0;
+}
